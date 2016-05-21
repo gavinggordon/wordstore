ae9f375d97c2ce9cec8adc340df36916871de786
Gavin G Gordon <gmarkowski@my.bcit.ca>
2016-05-21 14:03:38 -0700

    Updated class testing files
    Updated test files in accordance with change to the name of the main
    class file. This was done to simplify the overall use and the user
    readability of this class.

--- tests/WordStoreTest.php
+++ tests/WordStoreTest.php
@@ -1,30 +1,30 @@
 <?php
 
-use GGG\WordStore\Builder;
+use GGG\WordStore;
 
-class BuilderTest extends \PHPUnit_Framework_TestCase
+class WordStoreTest extends \PHPUnit_Framework_TestCase
 {
 
-	public $builder;
+	public $wordstore;
 	private $dictionaryFile;
 	
 	public function __construct()
 	{
 		$this->dictionaryFile = __DIR__ . DIRECTORY_SEPARATOR . 'dict.json';
-		$this->builder = new Builder( __DIR__ . DIRECTORY_SEPARATOR . 'dict.json' );
+		$this->wordstore = new WordStore( __DIR__ . DIRECTORY_SEPARATOR . 'dict.json' );
 		$this->assertFileExists( $this->dictionaryFile );
 	}
 
 	public function testAdd()
     {
-		$this->builder->add( 'noun', 'friend', ['translation'=>'vuča','gender'=>'conditional'] );
-		$word = $this->builder->find( 'friend' );
+		$this->wordstore->add( 'noun', 'friend', ['translation'=>'vuča','gender'=>'conditional'] );
+		$word = $this->wordstore->find( 'friend' );
         $this->assertCount( 1, $word );
     }
 	
 	public function testFind()
     {
-		$word = $this->builder->find( 'friend' );
+		$word = $this->wordstore->find( 'friend' );
 		$actual = $word[ 0 ]->translation;
 		$expected = 'vuča';
         $this->assertSame( $expected, $actual );
